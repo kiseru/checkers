dc9e9401644bb56dadc133a97b97283a16af2d64
alex97 <kis.alex97@yandex.ru>
2016-11-23 22:46:00 +0300

    Getters added in Cell

--- src/com/checkers/Cell.java
+++ src/com/checkers/Cell.java
@@ -9,10 +9,28 @@ public class Cell {
         this(_x, _y, null);
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Piece getPiece() throws NullPointerException {
+        if (piece == null) throw new NullPointerException();
+        else return piece;
+    }
+
+    public void setPiece(Piece piece) {
+        this.piece = piece;
+    }
+
     public Cell(int _x, int _y, Piece _piece) {
         x = _x;
         y = _y;
         piece = _piece;
+
     }
 
     public void show() {
