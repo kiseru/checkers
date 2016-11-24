51c6e69eafaa9954d3f0a6e18623353ed15ec2fa
Albina Galimzyanova <albina_kzn98@mail.ru>
2016-11-24 23:16:36 +0300

    Peace fixed

--- src/com/checkers/Piece.java
+++ src/com/checkers/Piece.java
@@ -3,10 +3,7 @@ package com.checkers;
 public abstract class Piece {
     private IUser user;
     private Colour colour;
-
-    abstract void move();
-
-    abstract void eat();
+    private boolean canEat;
 
     abstract void show();
 }
