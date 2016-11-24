e894be9dbf57adae34439b3d4ce4e9921fe1168e
Bulat <laineg2014@gmail.com>
2016-11-25 00:36:06 +0300

    setter, getter and constructor added

--- src/com/checkers/CheckerBoard.java
+++ src/com/checkers/CheckerBoard.java
@@ -1,5 +1,26 @@
 package com.checkers;
 
 public class CheckerBoard {
-    Cell[][] cells = new Cell[8][8];
+    private final int SIZE_OF_BOARD = 8;
+    private Cell[][] cells = new Cell[SIZE_OF_BOARD][SIZE_OF_BOARD];
+
+    public CheckerBoard(Cell[][] _cells) {
+        for (int i = 0; i < SIZE_OF_BOARD; i++) {
+            for (int j = 0; j < SIZE_OF_BOARD; j++) {
+                cells[i][j] = new Cell(_cells[i][j]);
+            }
+        }
+    }
+
+    public Cell[][] getCells() {
+        return cells;
+    }
+
+    public void setCells(Cell[][] _cells) {
+        for (int i = 0; i < SIZE_OF_BOARD; i++) {
+            for (int j = 0; j < SIZE_OF_BOARD; j++) {
+                cells[i][j] = new Cell(_cells[i][j]);
+            }
+        }
+    }
 }
