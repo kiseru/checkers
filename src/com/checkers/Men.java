f5b58c0eb4c6c766f1dcccbbd82649656570d958
alex97 <kis.alex97@yandex.ru>
2016-11-24 12:51:00 +0300

    Men fixed

--- src/com/checkers/Men.java
+++ src/com/checkers/Men.java
@@ -2,6 +2,16 @@ package com.checkers;
 
 public class Men extends Piece {
     @Override
+    void move(Cell to) {
+
+    }
+
+    @Override
+    void eat() {
+
+    }
+
+    @Override
     void show() {
         System.out.println('*');
     }
