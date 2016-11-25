b6b693544aba96abc2700580a5ea16c74cee31d1
alex97 <kis.alex97@yandex.ru>
2016-11-25 14:44:21 +0300

    Remove eat() and move() from Men

--- src/com/checkers/Men.java
+++ src/com/checkers/Men.java
@@ -2,16 +2,6 @@ package com.checkers;
 
 public class Men extends Piece {
     @Override
-    void move(Cell to) {
-
-    }
-
-    @Override
-    void eat() {
-
-    }
-
-    @Override
     void show() {
         System.out.println('*');
     }
