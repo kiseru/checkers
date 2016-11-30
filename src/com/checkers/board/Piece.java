9648a13d8b66e303ed1e799067fbc1b6460591ca
alex97 <kis.alex97@yandex.ru>
2016-11-30 19:20:57 +0300

    Delete canEat and canMove and their setters and getters from Piece

--- src/com/checkers/board/Piece.java
+++ src/com/checkers/board/Piece.java
@@ -5,24 +5,6 @@ import com.checkers.user.IUser;
 
 public abstract class Piece {
     private Colour colour;
-    private boolean canEat;
-    private boolean canMove;
-
-    public void setCanEat(boolean canEat) {
-        this.canEat = canEat;
-    }
-
-    public void setCanMove(boolean canMove) {
-        this.canMove = canMove;
-    }
-
-    public boolean isCanEat() {
-        return canEat;
-    }
-
-    public boolean isCanMove() {
-        return canMove;
-    }
 
     public void setColour(Colour colour) {
         this.colour = colour;
