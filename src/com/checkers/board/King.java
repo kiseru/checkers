e2e3e974272804b5b478763ecf95bc6ff59ec6c0
Bulat <laineg2014@gmail.com>
2016-12-08 23:49:49 +0300

    moving methods added

--- src/com/checkers/board/King.java
+++ src/com/checkers/board/King.java
@@ -11,7 +11,47 @@ public class King extends Piece {
 
     @Override
     public void analyzeAbilityOfMove() throws CheckersException {
-        //TODO write analyzeAbilityOfMove() method
+        boolean firstDirection = false;
+        boolean secondDirection = false;
+        boolean thirdDirection = false;
+        boolean forthDirection = false;
+        int i = 1;
+        while (getCell().getRow() + i <= 8 && getCell().getCol() + i <= 8 && !firstDirection) {
+            if (!isAbleToMoveTo(getCell().getNear(i, i))) {
+                firstDirection = false;
+                break;
+            }
+            firstDirection = isAbleToMoveTo(getCell().getNear(i, i));
+            i++;
+        }
+        i = 1;
+        while (getCell().getRow() + i <= 8 && getCell().getCol() - i >= 1 && !secondDirection) {
+            if (!isAbleToMoveTo(getCell().getNear(i, -i))) {
+                secondDirection = false;
+                break;
+            }
+            secondDirection = isAbleToMoveTo(getCell().getNear(i, -i));
+            i++;
+        }
+        i = 1;
+        while (getCell().getRow() - i >= 1 && getCell().getCol() + i <= 8 && !thirdDirection) {
+            if (!isAbleToMoveTo(getCell().getNear(-i, i))) {
+                thirdDirection = false;
+                break;
+            }
+            thirdDirection = isAbleToMoveTo(getCell().getNear(-i, i));
+            i++;
+        }
+        i = 1;
+        while (getCell().getRow() - i >= 1 && getCell().getCol() - i >= 1 && !forthDirection) {
+            if (!isAbleToMoveTo(getCell().getNear(-i, -i))) {
+                forthDirection = false;
+                break;
+            }
+            forthDirection = isAbleToMoveTo(getCell().getNear(-i, -i));
+            i++;
+        }
+        setCanMove(firstDirection || secondDirection || thirdDirection || forthDirection);
     }
 
     @Override
@@ -21,7 +61,50 @@ public class King extends Piece {
 
     @Override
     public boolean isAbleToMoveTo(Cell to) throws CheckersException {
-        //TODO write isAbleToMoveTo() method
+        Cell pieceCell = getCell();
+        if (pieceCell.diff(to) == -1) return false;
+        boolean firstDirection = false;
+        boolean secondDirection = false;
+        boolean thirdDirection = false;
+        boolean forthDirection = false;
+
+        int i = 1;
+        while (pieceCell.getRow() + i <= 8 && pieceCell.getCol() + i <= 8 && !firstDirection) {
+            if (pieceCell.getNear(i, i).getPiece() != null && pieceCell.getNear(i, i) != to) {
+                firstDirection = false;
+                break;
+            }
+            firstDirection = pieceCell.getNear(i, i) == to;
+            i++;
+        }
+        i = 1;
+        while (pieceCell.getRow() + i <= 8 && pieceCell.getCol() - i >= 1 && !secondDirection) {
+            if (pieceCell.getNear(i, -i).getPiece() != null && pieceCell.getNear(i, -i) != to) {
+                secondDirection = false;
+                break;
+            }
+            secondDirection = pieceCell.getNear(i, -i) == to;
+            i++;
+        }
+        i = 1;
+        while (pieceCell.getRow() - i >= 1 && pieceCell.getCol() + i <= 8 && !thirdDirection) {
+            if (pieceCell.getNear(-i, i).getPiece() != null && pieceCell.getNear(-i, i) != to) {
+                thirdDirection = false;
+                break;
+            }
+            thirdDirection = pieceCell.getNear(-i, i) == to;
+            i++;
+        }
+        i = 1;
+        while (pieceCell.getRow() - i >= 1 && pieceCell.getCol() - i >= 1 && !forthDirection) {
+            if (pieceCell.getNear(-i, -i).getPiece() != null && pieceCell.getNear(-i, -i) != to) {
+                forthDirection = false;
+                break;
+            }
+            forthDirection = pieceCell.getNear(-i, -i) == to;
+            i++;
+        }
+        return firstDirection || secondDirection || thirdDirection || forthDirection;
     }
 
     @Override
@@ -31,7 +114,12 @@ public class King extends Piece {
 
     @Override
     public void move(Cell to) throws CheckersException {
-        //TODO write move() method
+        if (isCanEat()) throw new CanEatException();
+        if (!isCanMove()) throw new CanNotMoveException();
+        if (!isAbleToMoveTo(to)) throw new CanNotMoveException();
+        Cell from = getCell();
+        to.setPiece(this);
+        from.setPiece(null);
     }
 
     @Override
