f21b28db9abc8ede5dea76b76351d1a65e6ecc5c
Alexandr Kiselev <kis.alex97@gmail.com>
2023-07-05 09:39:14 +0300

    removed BlackCellNotFoundException

--- src/main/java/com/checkers/exceptions/BlackCellNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.checkers.exceptions;
-
-import com.checkers.board.Cell;
-
-public class BlackCellNotFoundException extends CheckersException {
-    public BlackCellNotFoundException(Cell cell) {
-        msg = "You must make your turn only on black cells. " + cell.getMap() + "is not black";
-    }
-}
