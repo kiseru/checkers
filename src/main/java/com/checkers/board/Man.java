a0c2ddd5bdcf80e5d92e3449cf9af218c72f53fb
Alexandr Kiselev <kis.alex97@gmail.com>
2023-07-05 09:38:53 +0300

    refactored Man

--- src/main/java/com/checkers/board/Man.java
+++ src/main/java/com/checkers/board/Man.java
@@ -66,13 +66,19 @@ public class Man extends Piece {
             fourth = isAbleToEatTo(pieceCell.getNear(2, -2));
         } catch (ArrayIndexOutOfBoundsException ex) {}
 
-        if (first || second || third || fourth) setCanEat(true);
-        else setCanEat(false);
+        if (first || second || third || fourth) {
+            setCanEat(true);
+        } else {
+            setCanEat(false);
+        }
     }
 
     public boolean isAbleToMoveTo(Cell to) throws CheckersException {
         Cell pieceCell = getCell();
-        if (pieceCell.diff(to) != 1) return false;
+        if (pieceCell.diff(to) != 1) {
+            return false;
+        }
+
         boolean firstCell = false;
         boolean secondCell = false;
         if (getColor() == Color.WHITE) {
@@ -84,7 +90,9 @@ public class Man extends Piece {
                 secondCell = pieceCell.getNear(1, -1) == to;
             } catch (ArrayIndexOutOfBoundsException ex) {}
 
-            if (firstCell || secondCell) return true;
+            if (firstCell || secondCell) {
+                return true;
+            }
         } else {
             try {
                 firstCell = pieceCell.getNear(-1, -1) == to;
@@ -95,25 +103,47 @@ public class Man extends Piece {
                 secondCell = cell == to;
             } catch (ArrayIndexOutOfBoundsException ex) {}
 
-            if (firstCell || secondCell) return true;
+            if (firstCell || secondCell) {
+                return true;
+            }
         }
+
         return false;
     }
 
     public boolean isAbleToEatTo(Cell to) throws CheckersException {
         Cell pieceCell = getCell();
-        if (pieceCell.diff(to) != 2) return false;
-        else if (to.getPiece() != null) return false;
+        if (pieceCell.diff(to) != 2) {
+            return false;
+        } else if (to.getPiece() != null) {
+            return false;
+        }
+
         Cell target = pieceCell.between(to, pieceCell.getBoard());
-        if (target.getPiece() == null) return false;
-        if (target.getPiece().getColor() == getColor()) return false;
+        if (target.getPiece() == null) {
+            return false;
+        }
+
+        if (target.getPiece().getColor() == getColor()) {
+            return false;
+        }
+
         return true;
     }
 
     public void move(Cell to) throws CheckersException {
-        if (isCanEat()) throw new CanEatException();
-        if (!isCanMove()) throw new CanNotMoveException();
-        if (!isAbleToMoveTo(to)) throw new CanNotMoveException();
+        if (isCanEat()) {
+            throw new CanEatException();
+        }
+
+        if (!isCanMove()) {
+            throw new CanNotMoveException();
+        }
+
+        if (!isAbleToMoveTo(to)) {
+            throw new CanNotMoveException();
+        }
+
         Cell from = getCell();
         to.setPiece(this);
         from.setPiece(null);
@@ -126,8 +156,14 @@ public class Man extends Piece {
     }
 
     public void eat(Cell to) throws CheckersException {
-        if (!isCanEat()) throw new CanNotEatException();
-        if (!isAbleToEatTo(to)) throw new CanNotEatException();
+        if (!isCanEat()) {
+            throw new CanNotEatException();
+        }
+
+        if (!isAbleToEatTo(to)) {
+            throw new CanNotEatException();
+        }
+
         Cell from = getCell();
         to.setPiece(this);
         from.setPiece(null);
