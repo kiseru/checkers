c52b705e84c933959a4fc079bd5c4885febb7a87
Alexandr Kiselev <kis.alex97@gmail.com>
2023-07-05 09:39:53 +0300

    removed NothingToReadException.java

--- src/main/java/com/checkers/exceptions/NothingToReadException.java
+++
@@ -1,7 +0,0 @@
-package com.checkers.exceptions;
-
-public class NothingToReadException extends CheckersException {
-    public NothingToReadException() {
-        msg = "Can not read anything";
-    }
-}
