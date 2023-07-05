895dbba56b0bab74268293f405426cc9f3df8446
Alexandr Kiselev <kis.alex97@gmail.com>
2023-07-05 09:39:39 +0300

    removed CommandNotFoundException.java

--- src/main/java/com/checkers/exceptions/CommandNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.checkers.exceptions;
-
-public class CommandNotFoundException extends CheckersException {
-    public CommandNotFoundException() {
-        msg = "Such command has not found";
-    }
-}
