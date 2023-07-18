4e3f2afa1f7a27e266856d2da6db1753405735f4
kiseru <kis.alex97@gmail.com>
2023-07-18 19:55:41 +0300

    refactored servlets

--- src/main/java/com/checkers/servlets/GameServlet.java
+++ src/main/java/com/checkers/servlets/GameServlet.java
@@ -11,11 +11,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 @WebServlet("/game")
