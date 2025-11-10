75b3d268f1c5ecadc5da30ae6823ebb44a7cc536
Alexandr Kiselev <kis.alex97@gmail.com>
2025-11-10 18:25:57 +0300

    добавлено логирование в FinishController.kt

--- src/main/kotlin/ru/kiseru/checkers/controller/FinishController.kt
+++ src/main/kotlin/ru/kiseru/checkers/controller/FinishController.kt
@@ -1,14 +1,19 @@
 package ru.kiseru.checkers.controller
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping("finish")
+@RequestMapping("/finish")
 class FinishController {
 
+    private val logger = LoggerFactory.getLogger(FinishController::class.java)
+
     @GetMapping
-    fun getFinishPage(): String =
-        "finish"
+    fun getFinishPage(): String {
+        logger.info("Rendering refresh page")
+        return "finish"
+    }
 }
